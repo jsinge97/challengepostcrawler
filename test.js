f622bf9bb30d54c90425d36c3fdd149b8126cdda
Joshua Singer <josh.j.singer@gmail.com>
2015-07-26 15:54:10 -0500

    cleanup

--- test.js
+++
@@ -1,7 +0,0 @@
-baseUrl = 'http://sohacks2014.challengepost.com/submissions/';
-    $('li.next.next_page').filter().prev();
-
-    var select = $('li.next.next_page').filter().prev();
-    var pageNumber = 0;
-
-    console.log(select, pageNumber);
